00badecefb1ab344e3bd5da18d023122e010e654
MichaelSieghartsleitenr <s1510456035@students.fh-hagenberg.at>
2018-04-25 17:24:12 +0200

    form

--- index.php
+++ index.php
@@ -6,27 +6,30 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
 
     <title>Movie Lens</title>
 
 </head>
 <body>
+<div class="container">
 <h1>Movie Lens</h1>
-
-<form id="login" class = "form">
-    <label>
-        Login-ID:
-        <input type="text" placeholder="Login-ID" name = "login_id" value ="" required/>
-    </label>
-    <input type = "submit" value ="save">
+<p>Movie Lens is a great platform to get recommended movies, made with love ;).</p>
+<form id="login" class = "form-group">
+    <div class="form-group">
+        <label>Enter Login ID:</label>
+        <input type="text" placeholder="Login-ID" name = "login_id" required class="form-control"/>
+    </div>
+        <input type = "submit" value ="save" class="btn btn-info form-control">
 </form>
+</div>
 
 <!-- Optional JavaScript -->
 <!-- jQuery first, then Popper.js, then Bootstrap JS -->
 <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
 <script src="assets/js/code.js"></script>
 
 
